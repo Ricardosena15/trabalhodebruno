e84d05502c8be587a16681cfe81b5889ab1a5c47
Ricardosena125 <ricardo125gs@gmail.com>
2026-04-09 17:06:46 -0300

    Atualização

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,8 +10,8 @@ const router = createRouter({
       component: Home,
     },
     {
-      path: '/Apresentacao',
-      name: 'Apresentacao',
+      path: '/apresentacao',
+      name: 'apresentacao',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
